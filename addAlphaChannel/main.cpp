8dea09e1e54057c1c6726e328b2ee767e0288f97
Beau Johnston <beau@inbeta.org>
2011-08-29 13:34:26 +1000

    Initial Commit

---
+++ addAlphaChannel/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  addAlphaChannel
+//
+//  Created by Beau Johnston on 29/08/11.
+//  Copyright 2011 University Of New England. All rights reserved.
+//
+
+#include <iostream>
+
+int main (int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
